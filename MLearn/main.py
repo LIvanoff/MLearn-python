47ee7348d7879eb2f167eaf1844ff21b0fa52f53
LIvanoff <drfrily@gmail.com>
2023-02-16 02:01:10 +0300

    update main.py

--- MLearn/main.py
+++ MLearn/main.py
@@ -1,5 +1,10 @@
 from ARC.apriori import Apriori
+import time
 
+start = time.time()
 if __name__ == "__main__":
-    arc = Apriori(filename='data.json', min_length=3)
+    arc = Apriori(filename='data.json', min_length=2)
     arc.apriori(excel=True)
+
+end = time.time()
+print(" ", (end - start) * 10 ** 3, "ms")
