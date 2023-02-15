cd551d14e5f3fd14cbf219ecdbe9bcf2e6e2ecc9
LIvanoff <drfrily@gmail.com>
2023-02-15 21:59:25 +0300

    update apriori.py

--- MLearn/ARC/apriori.py
+++ MLearn/ARC/apriori.py
@@ -9,21 +9,29 @@ class Apriori(object):
     f3 = np.array([])
     transactions_count: int
     items_count: int
+    transactions: np.ndarray
+    unique_items: np.ndarray
 
     def __init__(self, filename: str, min_length: int):
         self.filename = filename
         self.min_length = min_length
         self.data = self.json_load()
         self.df = pd.DataFrame(self.data)
-        self.search_f1()
 
     def json_load(self):
         with open(self.filename, encoding="utf8") as f:
             return json.load(f)
 
-    def apriori(self):
+    def apriori(self, excel: bool = False):
+        self.search_f1()
         return
 
     def search_f1(self):
         self.transactions_count = len(self.df[self.df.columns[0]].unique())
         self.items_count = len(self.df[self.df.columns[1]].unique())
+        normal_matrix = np.zeros((self.transactions_count, self.items_count))
+
+        for i in range(self.items_count):
+            for j in range(self.transactions_count):
+                continue
+        print(normal_matrix)
