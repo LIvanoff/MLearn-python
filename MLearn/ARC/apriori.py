faafadd055b6e944a8a63260ba6bb319e12b07a6
LIvanoff <drfrily@gmail.com>
2023-02-16 01:07:19 +0300

    update apriori.py

--- MLearn/ARC/apriori.py
+++ MLearn/ARC/apriori.py
@@ -7,10 +7,15 @@ class Apriori(object):
     f1: np.ndarray
     f2: np.ndarray
     f3: np.ndarray
-    transactions: np.ndarray
-    unique_items: np.ndarray
+
+    transactions_list: np.ndarray
+    items_list: np.ndarray
+
     transactions_count: int
     items_count: int
+
+    transactions_unique: np.ndarray
+    items_unique: np.ndarray
 
     def __init__(self, filename: str, min_length: int):
         self.filename = filename
@@ -29,9 +34,25 @@ class Apriori(object):
     def search_f1(self):
         self.transactions_count = len(self.df[self.df.columns[0]].unique())
         self.items_count = len(self.df[self.df.columns[1]].unique())
+
         normal_matrix = np.zeros((self.transactions_count, self.items_count))
 
-        for i in range(self.items_count):
-            for j in range(self.transactions_count):
-                continue
+        self.transactions_list = self.df[self.df.columns[0]]
+        self.items_list = self.df[self.df.columns[1]]
+
+        self.transactions_unique = self.df[self.df.columns[0]].unique()
+        self.items_unique = self.df[self.df.columns[1]].unique()
+
+        tmp = ''
+        i = 0
+        j = 0
+        for transaction, item in zip(self.transactions_list, self.items_list):
+            if tmp != transaction:
+                i = np.where(self.transactions_unique == transaction)
+                tmp = transaction
+            j = np.where(self.items_unique == item)
+            normal_matrix[i][j] = 1
+            i = 0
+            j = 0
+            print(str(transaction) + " " + str(item))
         print(normal_matrix)
