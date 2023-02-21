31e0270c29cc58e77f4cd26aa5e301345738c56e
LIvanoff <drfrily@gmail.com>
2023-02-21 21:08:57 +0300

    add kmeans

---
+++ MLearn/clustering/KMeans.py
@@ -0,0 +1,20 @@
+
+
+class KMeans(object):
+    def __int__(self, metric: str = 'euclid_dist'):
+        self.metric_ = metric
+
+    def euclid_dist(self):
+        return
+
+    def manhattan_geom(self):
+        return
+
+    def chebyshev_dist(self):
+        return
+
+    def sq_euclid_dist(self):
+        return
+
+    def pow_dist(self):
+        return
