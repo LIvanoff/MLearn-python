824d691613b0a9687c6847f5b3882e1b22aa215d
LIvanoff <drfrily@gmail.com>
2023-02-22 01:42:03 +0300

    update kmeans

--- MLearn/clustering/KMeans.py
+++ MLearn/clustering/KMeans.py
@@ -11,20 +11,27 @@ class KMeans(object):
         self.clusters_ = clusters
         self.metric_ = metric
         self.max_iter_ = max_iter
-        self.labels = None
+        self.labels = np.array([])
 
     def train(self, X):
         centroids_index = np.arange(self.clusters_)
         centroids_index = np.random.choice(centroids_index, self.clusters_)
+
+        '''здесь будет switch для выбранной метрики'''
+        metric = self.euclid_dist
+
+        for x in X:
+            self.labels = np.append(self.labels, np.argmin(metric(x, centroids_index, X)))
+
         return
 
     def predict(self):
         return self.labels
 
-    def euclid_dist(self, X):
+    def euclid_dist(self, x, centroids_index, X):
         dist = np.array([])
-        for x, y in zip(X[:, 0], X[:, 1]):
-            dist = np.append(dist, math.sqrt(pow(x - y, 2)))
+        for y in centroids_index:
+            dist = np.append(dist, math.sqrt(pow(x[0] - X[], 2) * pow(x[1] - y, 2)))
         return dist
 
     def manhattan_geom(self, X):
