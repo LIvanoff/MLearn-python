a9f04ae7066914a81890e9df500563aa791f2491
Leonid <drfrily@gmail.com>
2023-02-22 02:27:09 +0300

    update kmeans

--- MLearn/clustering/KMeans.py
+++ MLearn/clustering/KMeans.py
@@ -8,45 +8,49 @@ class KMeans(object):
                  max_iter: int = 300,
                  stop_criteria: bool = True):
 
+        self.X = None
         self.clusters_ = clusters
         self.metric_ = metric
         self.max_iter_ = max_iter
         self.labels = np.array([])
 
     def train(self, X):
+        self.X = X
         centroids_index = np.arange(self.clusters_)
+        print(centroids_index)
         centroids_index = np.random.choice(centroids_index, self.clusters_)
+        print(centroids_index)
 
         '''здесь будет switch для выбранной метрики'''
         metric = self.euclid_dist
 
         for x in X:
-            self.labels = np.append(self.labels, np.argmin(metric(x, centroids_index, X)))
+            self.labels = np.append(self.labels, np.argmin(metric(x, centroids_index)))
 
         return
 
     def predict(self):
         return self.labels
 
-    def euclid_dist(self, x, centroids_index, X):
+    def euclid_dist(self, x, centroids_index):
         dist = np.array([])
-        for y in centroids_index:
-            dist = np.append(dist, math.sqrt(pow(x[0] - X[], 2) * pow(x[1] - y, 2)))
+        for y in self.X[centroids_index]:
+            dist = np.append(dist, math.sqrt(((x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2)))
         return dist
 
-    def manhattan_geom(self, X):
+    def manhattan_geom(self, x, centroids_index):
         dist = np.array([])
-        for x, y in zip(X[:, 0], X[:, 1]):
-            dist = np.append(dist, abs(x - y))
+        for y in self.X[centroids_index]:
+            dist = np.append(dist, abs((x[0] - y[0]) + (x[1] - y[1])))
         return dist
 
     def chebyshev_dist(self):
         return
 
-    def square_euclid_dist(self, X):
+    def square_euclid_dist(self, x, centroids_index):
         dist = np.array([])
-        for x, y in zip(X[:, 0], X[:, 1]):
-            dist = np.append(dist, pow(x - y, 2))
+        for y in self.X[centroids_index]:
+            dist = np.append(dist, (x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2)
         return dist
 
     def pow_dist(self):
