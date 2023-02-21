f74f1a3963eb823da7a49de70b006474db6ef208
LIvanoff <drfrily@gmail.com>
2023-02-21 22:35:11 +0300

    update kmeans

--- MLearn/clustering/KMeans.py
+++ MLearn/clustering/KMeans.py
@@ -1,8 +1,22 @@
+import numpy as np
 
 
 class KMeans(object):
-    def __int__(self, metric: str = 'euclid_dist'):
+    def __int__(self,
+                clusters: int,
+                metric: str = 'euclid_dist',
+                max_iter: int = 300,
+                stop_criteria: bool = False):
+
+        self.clusters_ = clusters
         self.metric_ = metric
+        self.max_iter_ = max_iter
+
+    def train(self):
+        return
+
+    def predict(self):
+        return
 
     def euclid_dist(self):
         return
@@ -17,4 +31,7 @@ class KMeans(object):
         return
 
     def pow_dist(self):
+        return
+
+    def stop_criteria(self):
         return
