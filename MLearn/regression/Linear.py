bbc7de286966969de5170f6b0dc28f3f14eb6b57
LIvanoff <drfrily@gmail.com>
2023-03-16 00:24:50 +0300

    update linear, add Adam

--- MLearn/regression/Linear.py
+++ MLearn/regression/Linear.py
@@ -12,8 +12,13 @@ class Linear(object):
     size_: int
     loss_history: np.ndarray
     pred: np.ndarray
-    beta1 = 0.9
-    beta2 = 0.999
+    beta1: float
+    beta2: float
+    EMA1_w: float
+    EMA1_b: float
+    EMA2_w: float
+    EMA2_b: float
+    t: int
 
     def __init__(self,
                  max_iter: int = 100,
@@ -21,8 +26,8 @@ class Linear(object):
                  learning_rate: float = 1 * pow(10, -3),
                  optimizer_name: str = "GD"
                  ):
-        self.weight_ = np.random.normal(loc=0.0, scale=0.1)
-        self.bias_ = np.random.normal(loc=0.0, scale=0.1)
+        self.weight_ = np.random.normal(loc=0.0, scale=0.01)
+        self.bias_ = np.random.normal(loc=0.0, scale=0.01)
         self.max_iter_ = max_iter
         self.stop_criteria_ = stop_criteria
         self.learning_rate_ = learning_rate
@@ -42,7 +47,6 @@ class Linear(object):
         self.loss_history = np.array([])
         for i in range(self.max_iter_):
             self.predict(self.X_)
-            self.plot()
             optimizer()
             loss = self.MSE()
             self.loss_history = np.append(self.loss_history, loss)
@@ -53,7 +57,7 @@ class Linear(object):
 
         plt.ioff()
         plt.show()
-        return
+        return self
 
     def MSE(self):
         return np.mean(np.power((self.Y_ - np.dot(self.X_, self.weight_) - self.bias_), 2))
@@ -81,8 +85,19 @@ class Linear(object):
             weight_deriv += -2 * self.X_[i] * (self.Y_[i] - (self.weight_ * self.X_[i] + self.bias_)) / self.size_
             bias_deriv += -2 * (self.Y_[i] - (self.weight_ * self.X_[i] + self.bias_)) / self.size_
 
-        self.weight_ -= self.learning_rate_ * 1 / np.sqrt(1) + epsilon
-        self.bias_ -= self.learning_rate_ * 1 / np.sqrt(1) + epsilon
+        self.EMA1_w = self.beta1 * self.EMA1_w + (1 - self.beta1) * weight_deriv
+        self.EMA1_b = self.beta1 * self.EMA1_b + (1 - self.beta1) * bias_deriv
+        self.EMA2_w = self.beta2 * self.EMA2_w + (1 - self.beta2) * np.power(weight_deriv, 2)
+        self.EMA2_b = self.beta2 * self.EMA2_b + (1 - self.beta2) * np.power(bias_deriv, 2)
+
+        self.EMA1_w = self.EMA1_w / (1 - np.power(self.beta1, self.t))
+        self.EMA1_b = self.EMA1_b / (1 - np.power(self.beta1, self.t))
+        self.EMA2_w = self.EMA2_w / (1 - np.power(self.beta2, self.t))
+        self.EMA2_b = self.EMA2_b / (1 - np.power(self.beta2, self.t))
+
+        self.weight_ -= self.learning_rate_ * (self.EMA1_w / np.sqrt(self.EMA2_w) + epsilon)
+        self.bias_ -= self.learning_rate_ * (self.EMA1_b / np.sqrt(self.EMA2_b) + epsilon)
+        self.t += 1
 
     def plot(self):
         plt.clf()
@@ -104,5 +119,11 @@ class Linear(object):
         if self.optimizer_name_ == 'SGD':
             return self.SGD()
         elif self.optimizer_name_ == 'Adam':
-            self.learning_rate_ = 3 * pow(10, -4)
-            return self.Adam()
+            self.beta1 = 0.9
+            self.beta2 = 0.999
+            self.EMA1_w = 0.0
+            self.EMA2_w = 0.0
+            self.EMA1_b = 0.0
+            self.EMA2_b = 0.0
+            self.t = 1
+            return self.Adam
