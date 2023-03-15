2d78faa7b581e4eb36477fb4af84e0ed884df255
LIvanoff <drfrily@gmail.com>
2023-03-15 22:16:56 +0300

    update linear

--- MLearn/regression/Linear.py
+++ MLearn/regression/Linear.py
@@ -12,11 +12,13 @@ class Linear(object):
     size_: int
     loss_history: np.ndarray
     pred: np.ndarray
+    beta1 = 0.9
+    beta2 = 0.999
 
     def __init__(self,
                  max_iter: int = 100,
                  stop_criteria: bool = True,
-                 learning_rate: float = 0.01,
+                 learning_rate: float = 1 * pow(10, -3),
                  optimizer_name: str = "GD"
                  ):
         self.weight_ = np.random.normal(loc=0.0, scale=0.1)
@@ -42,11 +44,10 @@ class Linear(object):
             self.predict(self.X_)
             self.plot()
             optimizer()
-            # Calculate cost for auditing purposes
             loss = self.MSE()
             self.loss_history = np.append(self.loss_history, loss)
             self.plot()
-            # Log Progress
+
             if i % 10 == 0:
                 print("iter: " + str(i) + " loss: " + str(loss))
 
@@ -74,27 +75,27 @@ class Linear(object):
     def Adam(self):
         weight_deriv = 0
         bias_deriv = 0
+        epsilon = pow(10, -8)
 
         for i in range(self.size_):
             weight_deriv += -2 * self.X_[i] * (self.Y_[i] - (self.weight_ * self.X_[i] + self.bias_)) / self.size_
             bias_deriv += -2 * (self.Y_[i] - (self.weight_ * self.X_[i] + self.bias_)) / self.size_
 
-        self.weight_ -= self.learning_rate_ * weight_deriv
-        self.bias_ -= self.learning_rate_ * bias_deriv
+        self.weight_ -= self.learning_rate_ * 1 / np.sqrt(1) + epsilon
+        self.bias_ -= self.learning_rate_ * 1 / np.sqrt(1) + epsilon
 
     def plot(self):
         plt.clf()
         plt.scatter(self.X_, self.Y_, marker='o', alpha=0.8)
         plt.plot(self.X_, self.pred, 'r')
-        plt.text(1, 2.5, 'y = ' + str(self.weight_) + ' * x + ' + str(self.bias_), fontsize=10, color='0.5')
+        plt.title('y = ' + str(self.weight_) + ' x + ' + str(self.bias_), fontsize=10, color='0.5')
         plt.draw()
         plt.gcf().canvas.flush_events()
-        # time.sleep(0.01)
+        time.sleep(0.01)
 
     def predict(self, X):
         self.pred = np.dot(X, self.weight_) + self.bias_
         return self.pred
-
 
     def stop_criteria(self):
         return
@@ -103,4 +104,5 @@ class Linear(object):
         if self.optimizer_name_ == 'SGD':
             return self.SGD()
         elif self.optimizer_name_ == 'Adam':
+            self.learning_rate_ = 3 * pow(10, -4)
             return self.Adam()
