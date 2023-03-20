0246249f7bc41c61a249430d57a06e3c89a21c2b
LIvanoff <drfrily@gmail.com>
2023-03-21 01:02:23 +0300

    update bayes

--- MLearn/bayes/bayesian_classifier.py
+++ MLearn/bayes/bayesian_classifier.py
@@ -1,4 +1,23 @@
+import numpy as np
 
 
 class Bayes(object):
-    def __init__(self):
+    '''
+    Класс метода наивного байесовского классификатора
+    '''
+    Y_: np.ndarray
+    X_: np.ndarray
+    parameters_num_: int
+    class_num_: int
+    condition_array_: np.ndarray
+
+    def __init__(self, parameters_num: int, class_num):
+        self.parameters_num_ = parameters_num
+        self.class_num_ = class_num
+
+    def fit(self, X, Y, condition_array):
+        self.X_ = X
+        self.Y_ = Y
+        self.condition_array_ = condition_array
+
+
