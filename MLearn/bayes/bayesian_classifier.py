d90a674756c328b9e0a8267b7b0f979d19cbd3e0
LIvanoff <drfrily@gmail.com>
2023-03-21 19:55:10 +0300

    update bayes

--- MLearn/bayes/bayesian_classifier.py
+++ MLearn/bayes/bayesian_classifier.py
@@ -14,14 +14,28 @@ class Bayes(object):
     def __init__(self, class_num: int):
         self.class_num_ = class_num
 
-    def fit(self, X, Y, condition_array):
+    def predict(self, X, Y, condition_array):
         self.X_ = X
         self.Y_ = Y
         self.condition_array_ = condition_array
+        self.class_probability = dict.fromkeys(np.unique(self.Y_), 1)
+        class_frequency = np.array([])
+
+        for key in self.class_probability.keys():
+            class_frequency = np.append(class_frequency, np.count_nonzero(self.Y_ == key))
 
         for i in range(len(condition_array)):
+            for key, j in zip(self.class_probability.keys(), range(self.class_num_)):
+                apriori_x = np.where(self.X_[:, i] == condition_array[i])[0]
+                apriori_y = np.where(self.Y_ == key)[0]
+                condition_frequency = np.count_nonzero(np.in1d(apriori_x, apriori_y) == True)
+                self.class_probability[key] *= condition_frequency / class_frequency[j]
 
-            for j in range(self.class_num_):
-                pass
+        for key, j in zip(self.class_probability.keys(), range(self.class_num_)):
+            self.class_probability[key] *= class_frequency[j] / len(self.Y_)
 
+        probability_sum = sum(self.class_probability.values())
+        for key in self.class_probability.keys():
+            self.class_probability[key] = self.class_probability[key] / probability_sum
 
+        return self.class_probability
