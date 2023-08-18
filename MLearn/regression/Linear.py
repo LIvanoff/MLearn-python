a9a0e2cfaf4891fda0546ed050896902f22f6b90
LIvanoff <drfrily@gmail.com>
2023-08-18 13:56:04 +0300

    update Linear.py

--- MLearn/regression/Linear.py
+++ MLearn/regression/Linear.py
@@ -31,26 +31,20 @@ import time
 class Linear(object):
     '''
     Класс метода численной линейной регрессии
-
-    X_           : Вектор объектов-признаков\n
-    Y_           : Вектор целевых значение\n
-    size_        : Размер выборки\n
+    size        : Размер выборки\n
     loss_history : История функции потерь\n
     max_iter_    : Максимальное количество итераций/количество эпох\n
-    batch_size_  : Рамзер батча\n
-    weight_      : Коэффициент угла наклона регрессии/перваый параметр\n
-    bias_        : Точка пересечения оси абцисс регрессией/второй параметр\n
+    batch_size  : Рамзер батча\n
+    weight      : Коэффициент угла наклона регрессии/перваый параметр\n
+    bias        : Точка пересечения оси абцисс регрессией/второй параметр\n
     pred         : Вектор предсказанных значений\n
     r1_score     : Коэффициент корреляции\n
     r2_score     : Коэффициент детерминации\n
     '''
-    Y_: np.ndarray
-    X_: np.ndarray
-    size_: int
+    size: int
     loss_history: np.ndarray
-    pred: np.ndarray
-    beta1_: float
-    beta2_: float
+    beta1: float
+    beta2: float
     EMA1_w: float
     EMA1_b: float
     EMA2_w: float
@@ -58,10 +52,10 @@ class Linear(object):
     t: int
     r2_score: float
     r1_score: float
-    weight_: float
-    bias_: float
+    weight: float
+    bias: float
     gradient: av.Variable
-    batch_size_: int
+    batch_size: int
     color: str
 
     def __init__(self,
@@ -74,59 +68,57 @@ class Linear(object):
                  beta2: float = 0.999,
                  batch_size: int = None,
                  ):
-        self.weight_ = np.random.normal(loc=0.0, scale=0.01)
-        self.bias_ = np.random.normal(loc=0.0, scale=0.01)
-        self.max_iter_ = max_iter
-        self.stop_criteria_ = stop_criteria
-        self.learning_rate_ = learning_rate
-        self.optimizer_name_ = optimizer_name
-        self.loss_function_ = loss_function
-        self.beta1_ = beta1
-        self.beta2_ = beta2
-        self.batch_size_ = batch_size
+        self.weight = np.random.normal(loc=0.0, scale=0.01)
+        self.bias = np.random.normal(loc=0.0, scale=0.01)
+        self.max_iter = max_iter
+        self.stop_criteria = stop_criteria
+        self.learning_rate = learning_rate
+        self.optimizer_name = optimizer_name
+        self.loss_function = loss_function
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.batch_size = batch_size
 
-    def fit(self, X, Y):
-        self.X_ = X
-        self.Y_ = Y
-        self.size_ = len(self.X_)
+    def fit(self, X, y):
+        size = len(X)
 
-        if self.optimizer_name_ == 'SGD':
+        if self.optimizer_name == 'SGD':
             optimize = self.SGD
             self.color = 'r'
         else:
             optimize = self.select_optimizer()
 
-        if self.loss_function_ == 'MSE':
+        if self.loss_function == 'MSE':
             loss_function = self.MSE
         else:
             loss_function = self.MAE
 
-        if self.batch_size_ is None:
-            self.batch_size_ = self.size_
+        if self.batch_size is None:
+            self.batch_size = size
 
         ad.set_mode('reverse')
         plt.ion()
         self.loss_history = np.array([])
 
-        for epoch in range(self.max_iter_):
-            order = np.random.permutation(len(self.X_))
+        for epoch in range(self.max_iter):
+            order = np.random.permutation(len(X))
 
-            for start_index in range(0, len(self.X_), self.batch_size_):
-                big_variable = av.Variable([self.weight_, self.bias_])
+            for start_index in range(0, len(X), self.batch_size):
+                big_variable = av.Variable([self.weight, self.bias])
                 weight, bias = big_variable[0], big_variable[1]
 
-                batch_indexes = order[start_index:start_index + self.batch_size_]
+                batch_indexes = order[start_index:start_index + self.batch_size]
 
-                X_batch = self.X_[batch_indexes]
-                y_batch = self.Y_[batch_indexes]
+                X_batch = X[batch_indexes]
+                y_batch = y[batch_indexes]
 
-                self.forward(weight, bias, X_batch)
+                pred = self.forward(weight, bias, X_batch)
 
-                loss_value = loss_function(y_batch)
+                loss_value = loss_function(pred, y_batch)
                 self.gradient = loss_value.compute_gradients()
                 optimize()
                 self.loss_history = np.append(self.loss_history, loss_value.data)
-                self.plot()
+                self.plot(X, y, pred)
                 ad.reset_graph()
 
                 if epoch % 10 == 0:
@@ -136,50 +128,49 @@ class Linear(object):
         plt.show()
         return self
 
-    def MSE(self, Y):
-        return np.mean(np.power((Y - self.pred), 2))
+    def MSE(self, pred, y):
+        return np.mean(np.power((pred - y), 2))
 
-    def RMSE(self, Y):
-        return np.sqrt(np.mean(np.power((Y - self.pred), 2)))
+    def RMSE(self, pred, y):
+        return np.sqrt(np.mean(np.power((pred - y), 2)))
 
-    def MAE(self, Y):
-        return np.mean(np.abs(Y - self.pred))
+    def MAE(self, pred, y):
+        return np.mean(np.abs(pred - y))
 
     def SGD(self):
-        self.weight_ -= self.learning_rate_ * self.gradient[0][0]
-        self.bias_ -= self.learning_rate_ * self.gradient[0][1]
+        self.weight -= self.learning_rate * self.gradient[0][0]
+        self.bias -= self.learning_rate * self.gradient[0][1]
 
     def RMSprop(self):
         epsilon = pow(10, -8)
 
-        self.EMA1_w = self.beta1_ * self.EMA1_w + (1 - self.beta1_) * np.power(self.gradient[0][0], 2)
-        self.EMA1_b = self.beta1_ * self.EMA1_b + (1 - self.beta1_) * np.power(self.gradient[0][1], 2)
+        self.EMA1_w = self.beta1 * self.EMA1_w + (1 - self.beta1) * np.power(self.gradient[0][0], 2)
+        self.EMA1_b = self.beta1 * self.EMA1_b + (1 - self.beta1) * np.power(self.gradient[0][1], 2)
 
-        self.weight_ -= self.learning_rate_ * self.gradient[0][0] / np.sqrt(self.EMA1_w + epsilon)
-        self.bias_ -= self.learning_rate_ * self.gradient[0][1] / np.sqrt(self.EMA1_b + epsilon)
+        self.weight -= self.learning_rate * self.gradient[0][0] / np.sqrt(self.EMA1_w + epsilon)
+        self.bias -= self.learning_rate * self.gradient[0][1] / np.sqrt(self.EMA1_b + epsilon)
 
     def Adam(self):
         epsilon = pow(10, -8)
 
-        self.EMA1_w = self.beta1_ * self.EMA1_w + (1 - self.beta1_) * self.gradient[0][0] / (
-                    1 - np.power(self.beta1_, self.t))
-        self.EMA1_b = self.beta1_ * self.EMA1_b + (1 - self.beta1_) * self.gradient[0][1] / (
-                    1 - np.power(self.beta1_, self.t))
-        self.EMA2_w = self.beta2_ * self.EMA2_w + (1 - self.beta2_) * np.power(self.gradient[0][0], 2) / (
-                    1 - np.power(self.beta2_, self.t))
-        self.EMA2_b = self.beta2_ * self.EMA2_b + (1 - self.beta2_) * np.power(self.gradient[0][1], 2) / (
-                    1 - np.power(self.beta2_, self.t))
+        self.EMA1_w = self.beta1 * self.EMA1_w + (1 - self.beta1) * self.gradient[0][0] / (
+                    1 - np.power(self.beta1, self.t))
+        self.EMA1_b = self.beta1 * self.EMA1_b + (1 - self.beta1) * self.gradient[0][1] / (
+                    1 - np.power(self.beta1, self.t))
+        self.EMA2_w = self.beta2 * self.EMA2_w + (1 - self.beta2) * np.power(self.gradient[0][0], 2) / (
+                    1 - np.power(self.beta2, self.t))
+        self.EMA2_b = self.beta2 * self.EMA2_b + (1 - self.beta2) * np.power(self.gradient[0][1], 2) / (
+                    1 - np.power(self.beta2, self.t))
 
-        self.weight_ -= self.learning_rate_ * self.EMA1_w / (np.sqrt(self.EMA2_w) + epsilon)
-        self.bias_ -= self.learning_rate_ * self.EMA1_b / (np.sqrt(self.EMA2_b) + epsilon)
+        self.weight -= self.learning_rate * self.EMA1_w / (np.sqrt(self.EMA2_w) + epsilon)
+        self.bias -= self.learning_rate * self.EMA1_b / (np.sqrt(self.EMA2_b) + epsilon)
         self.t += 1
 
-    def plot(self):
-        self.predict(self.X_)
+    def plot(self, X, y, pred):
         plt.clf()
-        plt.scatter(self.X_, self.Y_, marker='o', alpha=0.8)
-        plt.plot(self.X_, self.pred, color=self.color, label=str(self.optimizer_name_))
-        plt.title('y = ' + str(self.weight_) + ' x + ' + str(self.bias_) + ' + ' + str(self.loss_history[-1]),
+        plt.scatter(X, y, marker='o', alpha=0.8)
+        plt.plot(X, pred, color=self.color, label=str(self.optimizer_name))
+        plt.title('y = ' + str(self.weight) + ' x + ' + str(self.bias) + ' + ' + str(self.loss_history[-1]),
                   fontsize=10, color='0.5')
         plt.legend()
         plt.draw()
@@ -187,18 +178,18 @@ class Linear(object):
         time.sleep(0.0001)
 
     def predict(self, X):
-        self.pred = np.dot(X, self.weight_) + self.bias_
-        return self.pred
+        pred = np.dot(X, self.weight) + self.bias
+        return pred
 
     def forward(self, weight, bias, X):
-        self.pred = np.dot(X, weight) + bias
-        return self.pred
+        pred = np.dot(X, weight) + bias
+        return pred
 
     def stop_criteria(self):
         return
 
     def select_optimizer(self):
-        if self.optimizer_name_ == 'Adam':
+        if self.optimizer_name == 'Adam':
             self.EMA1_w = 0.0
             self.EMA2_w = 0.0
             self.EMA1_b = 0.0
@@ -206,7 +197,7 @@ class Linear(object):
             self.t = 1
             self.color = 'b'
             return self.Adam
-        elif self.optimizer_name_ == 'RMSprop':
+        elif self.optimizer_name == 'RMSprop':
             self.EMA1_w = 0.0
             self.EMA1_b = 0.0
             self.color = 'g'
